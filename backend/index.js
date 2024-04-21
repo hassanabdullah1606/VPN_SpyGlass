2ddd817feb3459702d9321ea64af271237edf492
Hassan Abdullah <hassanabdullah1606@yahoo.com>
2024-04-21 17:48:16 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,34 @@ app.get('/api/network-packets', async (req, res) => {
   }
 });
 
-// Other routes...
+app.get('/api/network-packets-current-date', async (req, res) => {
+  try {
+    const currentDate = new Date().toISOString().split('T')[0];
+    const packets = await NetworkPacket.find({
+      timestamp: {
+        $gte: new Date(currentDate),
+        $lt: new Date(currentDate + 'T23:59:59.999Z'),
+      },
+    });
+
+    res.json(packets);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).json({ error: 'Error fetching data' });
+  }
+});
+
+app.post('/api/network-packets', async (req, res) => {
+  try {
+    const newPacketData = req.body;
+    const createdPacket = await NetworkPacket.create(newPacketData);
+    res.status(201).json(createdPacket);
+  } catch (err) {
+    console.error('Error creating new packet:', err);
+    res.status(500).json({ error: 'Error creating new packet' });
+  }
+});
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => {
