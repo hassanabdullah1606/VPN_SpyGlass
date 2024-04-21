99acdc94951b98082b8bce0640b80cc5c54f41b3
Hassan Abdullah <hassanabdullah1606@yahoo.com>
2024-04-21 15:01:22 +0500

    vercel.json added

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,11 @@ const cors = require('cors');
 const NetworkPacket = require('./models/TrafficModel');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: ["https://vpnspyglass-api.vercel.app"],
+  methods: ["POST","GET"],
+  credentials: true
+}));
 
 const dbURI = process.env.DB_URI;
 
