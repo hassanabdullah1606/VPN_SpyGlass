9159c65994f6fb2060496ab73e660431aba8bc36
Hassan Abdullah <hassanabdullah1606@yahoo.com>
2024-04-22 02:09:45 +0500

    Update Traffic.tsx

--- frontend/src/components/Traffic/Traffic.tsx
+++ frontend/src/components/Traffic/Traffic.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { topVPNsUsers } from '../../data';
 import './Traffic.scss';
 
