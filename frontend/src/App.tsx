5bec61433cd8298ebf2a93a873ead904df0c2af1
Hassan Abdullah <hassanabdullah1606@yahoo.com>
2024-04-30 17:07:28 +0500

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,49 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import Home from './pages/home/Home';
 import Faq from './pages/FAQ/Faq';
 import Settings from './pages/settings/Settings';
 import Login from './pages/login/Login';
-import './styles/global.scss';
-import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Signup from './pages/Signup/Signup';
+import './styles/global.scss';
 
 function App() {
-  const Layout = () => {
-    return (
-      <div className="main">
-        <Outlet />
-      </div>
-    );
-  };
-  const router = createBrowserRouter([
-    {
-      path: '/',
-      element: <Layout />,
-      children: [
-        {
-          path: '/',
-          element: <Login />,
-        },
-        {
-          path: '/settings',
-          element: <Settings />,
-        },
-        {
-          path: '/faq',
-          element: <Faq />,
-        },
-        {
-          path: '/home',
-          element: <Home />,
-        },
-        {
-          path: '/signup',
-          element: <Signup />,
-        },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+    <div className="main">
+      <Router> {/* Wrap your Routes in a BrowserRouter */}
+        <Routes> {/* Use Routes component to define your routes */}
+          <Route path="/" element={<Login />} /> {/* Define a route for Login */}
+          <Route path="/settings" element={<Settings />} /> {/* Define a route for Settings */}
+          <Route path="/faq" element={<Faq />} /> {/* Define a route for FAQ */}
+          <Route path="/home" element={<Home />} /> {/* Define a route for Home */}
+          <Route path="/signup" element={<Signup />} /> {/* Define a route for Signup */}
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
